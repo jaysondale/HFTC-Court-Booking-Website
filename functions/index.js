b3871bd546382f94d8c1d1e423434b984c08a24c
Jayson Dale <jaysondale1@gmail.com>
2020-06-02 14:51:11 -0400

    Fixed login logo image

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,1 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp();
-
-// Verify if current user is an Administrator
-exports.checkAdmin = functions.database.ref('/special_bookings').onCreate((snap, context) => {
-    if (context.authType === 'ADMIN') {
-        
-    }
-})
+// No cloud functions
